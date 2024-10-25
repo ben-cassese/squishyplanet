fb7f7c6ec3087ec7416958219d2c01db10cc987f
Ben-Cassese <b.c.cassese@columbia.edu>
2024-10-25 13:11:51 -0400

    added tests

---
+++ tests/test_limb_darkening.py
@@ -0,0 +1,38 @@
+import jax
+
+jax.config.update("jax_enable_x64", True)
+import jax.numpy as jnp
+
+from squishyplanet.limb_darkening_laws import (
+    linear_ld_law,
+    quadratic_ld_law,
+    kipping_ld_law,
+    squareroot_ld_law,
+    nonlinear_3param_ld_law,
+    nonlinear_4param_ld_law,
+)
+
+
+def test_limb_darkening_laws():
+    mus = jnp.linspace(0, 1, 100)
+
+    coeffs = linear_ld_law(u1=0.5)
+    assert jnp.allclose(coeffs, jnp.array([0.5, 0.0]))
+    _ = linear_ld_law(u1=0.5, return_profile=True)
+
+    coeffs = quadratic_ld_law(u1=0.5, u2=0.2)
+    assert jnp.allclose(coeffs, jnp.array([0.5, 0.2]))
+    _ = quadratic_ld_law(u1=0.5, u2=0.2, return_profile=True)
+
+    coeffs = kipping_ld_law(q1=0.5, q2=0.2)
+    assert jnp.allclose(coeffs, jnp.array([0.28284271, 0.42426407]))
+    _ = kipping_ld_law(q1=0.5, q2=0.2, return_profile=True)
+
+    coeffs = squareroot_ld_law(u1=0.5, u2=0.2)
+    _ = squareroot_ld_law(u1=0.5, u2=0.2, return_profile=True)
+
+    coeffs = nonlinear_3param_ld_law(u1=0.5, u2=0.2, u3=0.1)
+    _ = nonlinear_3param_ld_law(u1=0.5, u2=0.2, u3=0.1, return_profile=True)
+
+    coeffs = nonlinear_4param_ld_law(u1=0.5, u2=0.2, u3=0.1, u4=0.05)
+    _ = nonlinear_4param_ld_law(u1=0.5, u2=0.2, u3=0.1, u4=0.05, return_profile=True)
