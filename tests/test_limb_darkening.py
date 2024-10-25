e3cb269e38a6c2254aad9469f392a05e2070678d
Ben-Cassese <b.c.cassese@columbia.edu>
2024-10-25 13:14:02 -0400

    added comparison test

--- tests/test_limb_darkening.py
+++ tests/test_limb_darkening.py
@@ -18,21 +18,47 @@ def test_limb_darkening_laws():
 
     coeffs = linear_ld_law(u1=0.5)
     assert jnp.allclose(coeffs, jnp.array([0.5, 0.0]))
-    _ = linear_ld_law(u1=0.5, return_profile=True)
+    profile = linear_ld_law(u1=0.5, return_profile=True)
+    diff = (
+        profile["true_intensity_profile"] - profile["squishyplanet_intensity_profile"]
+    )
+    assert jnp.max(jnp.abs(diff)) < 0.1
 
     coeffs = quadratic_ld_law(u1=0.5, u2=0.2)
     assert jnp.allclose(coeffs, jnp.array([0.5, 0.2]))
-    _ = quadratic_ld_law(u1=0.5, u2=0.2, return_profile=True)
+    profile = quadratic_ld_law(u1=0.5, u2=0.2, return_profile=True)
+    diff = (
+        profile["true_intensity_profile"] - profile["squishyplanet_intensity_profile"]
+    )
+    assert jnp.max(jnp.abs(diff)) < 0.1
 
     coeffs = kipping_ld_law(q1=0.5, q2=0.2)
     assert jnp.allclose(coeffs, jnp.array([0.28284271, 0.42426407]))
-    _ = kipping_ld_law(q1=0.5, q2=0.2, return_profile=True)
+    profile = kipping_ld_law(q1=0.5, q2=0.2, return_profile=True)
+    diff = (
+        profile["true_intensity_profile"] - profile["squishyplanet_intensity_profile"]
+    )
+    assert jnp.max(jnp.abs(diff)) < 0.1
 
     coeffs = squareroot_ld_law(u1=0.5, u2=0.2)
-    _ = squareroot_ld_law(u1=0.5, u2=0.2, return_profile=True)
+    profile = squareroot_ld_law(u1=0.5, u2=0.2, return_profile=True)
+    diff = (
+        profile["true_intensity_profile"] - profile["squishyplanet_intensity_profile"]
+    )
+    assert jnp.max(jnp.abs(diff)) < 0.1
 
     coeffs = nonlinear_3param_ld_law(u1=0.5, u2=0.2, u3=0.1)
-    _ = nonlinear_3param_ld_law(u1=0.5, u2=0.2, u3=0.1, return_profile=True)
+    profile = nonlinear_3param_ld_law(u1=0.5, u2=0.2, u3=0.1, return_profile=True)
+    diff = (
+        profile["true_intensity_profile"] - profile["squishyplanet_intensity_profile"]
+    )
+    assert jnp.max(jnp.abs(diff)) < 0.1
 
     coeffs = nonlinear_4param_ld_law(u1=0.5, u2=0.2, u3=0.1, u4=0.05)
-    _ = nonlinear_4param_ld_law(u1=0.5, u2=0.2, u3=0.1, u4=0.05, return_profile=True)
+    profile = nonlinear_4param_ld_law(
+        u1=0.5, u2=0.2, u3=0.1, u4=0.05, return_profile=True
+    )
+    diff = (
+        profile["true_intensity_profile"] - profile["squishyplanet_intensity_profile"]
+    )
+    assert jnp.max(jnp.abs(diff)) < 0.1
