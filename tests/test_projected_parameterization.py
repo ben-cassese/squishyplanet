afaeb4f1d99ab4f4ed80dde6dde59718b34b328e
Ben-Cassese <b.c.cassese@columbia.edu>
2024-10-26 14:50:24 -0400

    avoid recompilation within test

--- tests/test_projected_parameterization.py
+++ tests/test_projected_parameterization.py
@@ -6,6 +6,7 @@ import jax.numpy as jnp
 from squishyplanet import OblateSystem
 
 import astropy.units as u
+from tqdm import tqdm
 
 
 def test_projected_parameterization():
@@ -13,7 +14,48 @@ def test_projected_parameterization():
     t_exp = 5 * u.min
     times = jnp.arange(-30, 30, t_exp.to(u.hour).value) * u.hour.to(u.day)
 
-    for i in range(2_000):
+    key, *subkeys = jax.random.split(key, (5,))
+
+    # the 3d planet
+    injected_state = {
+        "t_peri": -1071.23205 / 4,
+        "times": times,
+        "exposure_time": t_exp.to(u.day).value,
+        "oversample": 5,  # 5x more samples under-the-hood, then binned back down
+        "oversample_correction_order": 2,
+        "a": 540.0,
+        "period": 1071.23205,
+        "r": 0.1,
+        "i": 89.9720 * jnp.pi / 180,
+        "ld_u_coeffs": jnp.array([0.4, 0.26]),
+        "f1": jax.random.uniform(subkeys[0], (1,), minval=0.0, maxval=0.99),
+        "f2": jax.random.uniform(subkeys[1], (1,), minval=0.0, maxval=0.99),
+        "obliq": jax.random.uniform(subkeys[2], (1,), minval=0.0, maxval=jnp.pi),
+        "prec": jax.random.uniform(subkeys[3], (1,), minval=0.0, maxval=jnp.pi),
+        "tidally_locked": False,
+    }
+    planet1 = OblateSystem(**injected_state)
+
+    # the 2d planet
+    injected_state2 = {
+        "t_peri": -1071.23205 / 4,
+        "times": times,
+        "exposure_time": t_exp.to(u.day).value,
+        "oversample": 5,  # 5x more samples under-the-hood, then binned back down
+        "oversample_correction_order": 2,
+        "a": 540.0,
+        "period": 1071.23205,
+        "i": 89.9720 * jnp.pi / 180,
+        "ld_u_coeffs": jnp.array([0.4, 0.26]),
+        "tidally_locked": False,
+        "parameterize_with_projected_ellipse": True,
+        "projected_effective_r": planet1.state["projected_effective_r"][0],
+        "projected_f": planet1.state["projected_f"],
+        "projected_theta": planet1.state["projected_theta"],
+    }
+    planet2 = OblateSystem(**injected_state2)
+
+    for i in tqdm(range(2_000)):
         key, *subkeys = jax.random.split(key, (5,))
 
         # the 3d planet
@@ -34,30 +76,25 @@ def test_projected_parameterization():
             "prec": jax.random.uniform(subkeys[3], (1,), minval=0.0, maxval=jnp.pi),
             "tidally_locked": False,
         }
+        params = {
+            "f1": jax.random.uniform(subkeys[0], (1,), minval=0.0, maxval=0.99),
+            "f2": jax.random.uniform(subkeys[1], (1,), minval=0.0, maxval=0.99),
+            "obliq": jax.random.uniform(subkeys[2], (1,), minval=0.0, maxval=jnp.pi),
+            "prec": jax.random.uniform(subkeys[3], (1,), minval=0.0, maxval=jnp.pi),
+        }
+        # create a dummy planet to go through __init__ but avoid recompiling lightcurve
+        _tmp_planet = OblateSystem(**injected_state)
 
-        planet1 = OblateSystem(**injected_state)
-        lc1 = planet1.lightcurve()
+        lc1 = planet1.lightcurve(params)
 
         # the 2d planet
         injected_state2 = {
-            "t_peri": -1071.23205 / 4,
-            "times": times,
-            "exposure_time": t_exp.to(u.day).value,
-            "oversample": 5,  # 5x more samples under-the-hood, then binned back down
-            "oversample_correction_order": 2,
-            "a": 540.0,
-            "period": 1071.23205,
-            "i": 89.9720 * jnp.pi / 180,
-            "ld_u_coeffs": jnp.array([0.4, 0.26]),
-            "tidally_locked": False,
-            "parameterize_with_projected_ellipse": True,
-            "projected_effective_r": planet1.state["projected_effective_r"][0],
-            "projected_f": planet1.state["projected_f"],
-            "projected_theta": planet1.state["projected_theta"],
+            "projected_effective_r": _tmp_planet._state["projected_effective_r"][0],
+            "projected_f": _tmp_planet._state["projected_f"],
+            "projected_theta": _tmp_planet._state["projected_theta"],
         }
 
-        planet2 = OblateSystem(**injected_state2)
-        lc2 = planet2.lightcurve()
+        lc2 = planet2.lightcurve(injected_state2)
 
         # looking out for spikes
         assert jnp.all(lc1 <= 1.0)
